64e1808be5acfae9bc37559812ad75bcdd5aabb2
coryo12345 <corydio12@gmail.com>
2026-01-29 20:05:00 -0500

    setup for ui dev

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(), 
+      tailwindcss(),
+      Components({
+        resolvers: [
+          PrimeVueResolver()
+        ]
+      })
+    ],
     build: {
       outDir:
         mode == "production"
