f9c6451c586be83925fa7cdf7f53eba7095e52dd
coryo12345 <corydio12@gmail.com>
2026-02-08 00:37:00 -0500

    speed spell

---
+++ src/main/java/servermagic/spells/SpeedSpell.java
@@ -0,0 +1,30 @@
+package servermagic.spells;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+
+public class SpeedSpell extends BaseSpell {
+
+    public SpeedSpell(ServerLevel world, ServerPlayer player) {
+        super(world, player);
+    }
+
+    @Override
+    protected void spellImplementation() {
+        // 30 seconds = 30 * 20 ticks = 600 ticks
+        // Amplifier 0 is Speed I, Amplifier 1 is Speed II, etc.
+        player.addEffect(new MobEffectInstance(MobEffects.SPEED, 600, 0));
+    }
+
+    @Override
+    public String displayName() {
+        return "Speed";
+    }
+
+    @Override
+    public String description() {
+        return "Gives you speed for 30 seconds";
+    }
+}
