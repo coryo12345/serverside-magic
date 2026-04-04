b48bd7e10073153dd301555df34d2d7f4e2b440d
coryo12345 <corydio12@gmail.com>
2026-04-04 14:02:35 -0400

    standard text character

--- src/main/java/servermagic/web/skill/Skills.java
+++ src/main/java/servermagic/web/skill/Skills.java
@@ -146,7 +146,7 @@ public class Skills {
         public static final Skill EARTHEN_SPIKE = new Skill("earthen_spike", "Earthen Spike",
                         "Summon a line of earth spikes in front of you", FIREBOLT.id());
         public static final Skill SPECTRAL_HAMMER = new Skill("spectral_hammer", "Spectral Hammer",
-                        "Summon a spectral hammer that smashes a 3×3 face of blocks", EARTHEN_SPIKE.id());
+                        "Summon a spectral hammer that smashes a 3x3 face of blocks", EARTHEN_SPIKE.id());
 
         public static final Skill SECRETS = new Skill("secrets", "Secrets", "secret skills", null);
         public static final Skill ANGEL_WINGS = new Skill("angel_wings", "Angel Wings", "Summon wings and fly!",
